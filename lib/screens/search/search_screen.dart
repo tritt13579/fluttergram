8b6dd173bbb613f01a7f939e7b12ebd330e0e126
HuyHmi <huynhhuy.079996@gmail.com>
2025-05-09 11:45:55 +0700

    update

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttergram/screens/stories/stories_screen.dart';
 
@@ -9,6 +10,15 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController searchController = TextEditingController();
+  bool isShowUsers = false;
+
+  void onSearchSubmitted(String query) {
+    setState(() {
+      isShowUsers = query.isNotEmpty;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final List<String> imageUrls = List.generate(
@@ -18,9 +28,10 @@ class _SearchScreenState extends State<SearchScreen> {
     return Scaffold(
       appBar: AppBar(
         title: TextField(
+          controller: searchController,
           keyboardType: TextInputType.text,
           decoration: InputDecoration(
-            hintText: 'Search',
+            hintText: 'Tìm kiếm',
             prefixIcon: Icon(Icons.search),
             border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(13),
@@ -30,15 +41,63 @@ class _SearchScreenState extends State<SearchScreen> {
             fillColor: Colors.grey,
             contentPadding: EdgeInsets.zero,
           ),
+          onSubmitted: onSearchSubmitted,
+          onChanged: (value) {
+            if (value.isEmpty && isShowUsers) {
+              setState(() {
+                isShowUsers = false;
+              });
+            }
+          },
         ),
         backgroundColor: Colors.black,
         elevation: 1,
       ),
-      body: Padding(
+      body: isShowUsers? FutureBuilder(
+          future: FirebaseFirestore.instance.collection('users')
+          .where('username', isGreaterThanOrEqualTo: searchController.text)
+              .get(),
+          builder: (context, snapshot) {
+            if(!snapshot.hasData) {
+              return Center(child: CircularProgressIndicator(),);
+            }
+            final QuerySnapshot<Map<String, dynamic>> data =
+            snapshot.data as QuerySnapshot<Map<String, dynamic>>;
+            final List<QueryDocumentSnapshot<Map<String, dynamic>>> users = data.docs;
+            // if(users.isEmpty) {
+            //
+            // }
+            return ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  final user = users[index].data();
+                  return ListTile(
+                    leading: (user['avatar_url'] != null && user['avatar_url'].toString().isNotEmpty)
+                        ? CircleAvatar(
+                      backgroundImage: NetworkImage(user['avatar_url']),
+                    )
+                        : const CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      child: Icon(Icons.person, color: Colors.white),
+                    ),
+
+                    title: Text(
+                      user['username'],
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onTap: () {
+
+                    },
+                  );
+                },
+            );
+          },
+      )
+      : Padding(
         padding: const EdgeInsets.all(4.0),
         child: GridView.builder(
           itemCount: imageUrls.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 3,
             crossAxisSpacing: 4,
             mainAxisSpacing: 4,
@@ -50,17 +109,6 @@ class _SearchScreenState extends State<SearchScreen> {
             );
           },
         ),
-        // child: Center(
-        //   child: ElevatedButton(
-        //     onPressed: () {
-        //       Navigator.push(
-        //         context,
-        //         MaterialPageRoute(builder: (context) => StoriesScreen()),
-        //       );
-        //     },
-        //     child: Text("Page Story"),
-        //   ),
-        // ),
       ),
     );
   }
